00c7cdf2671a15bf663bab6b7719299fbb6de4b5
Gabriel Freites <gabohd@gmail.com>
2023-10-31 19:14:11 +0100

    finished the guessing game in a better way

--- rust-book/guessing-game/src/main.rs
+++ rust-book/guessing-game/src/main.rs
@@ -1,11 +1,11 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    
+
     loop {
         println!("Please input your guess.");
         let mut guess = String::new();
@@ -13,11 +13,14 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
         if guess.trim().to_lowercase().eq("quit") {
-          break
+            break;
         }
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                println!("Try guessing a number!");
+                continue;
+            }
         };
         println!("You guessed: {guess}");
 
